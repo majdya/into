f99ed96e54b1c071f81ac17952de11910bc49849
majdyaq <majd@feelit.tech>
2024-02-17 15:40:31 +0200

    user module and  controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query,ParseIntPipe,ValidationPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  
+  // eslint-disable-next-line prettier/prettier
+  constructor(private readonly usersService: UsersService){}
+
+  @Get() //GET /users or /users?role=value
+  // eslint-disable-next-line prettier/prettier
+  findAll(@Query('role') role?: 'Intern' | 'Admin' | 'Experienced'): any[] {
+    return this.usersService.findAll(role);
+  }
+
+  @Get(':id') //GET /users/:id
+  findOne(@Param('id',ParseIntPipe) id: number) {
+    return this.usersService.findOne(id)
+  }
+
+  @Post() //POST /users
+  create(@Body(ValidationPipe) createUserDto: CreateUserDto){
+    return this.usersService.create(createUserDto);
+  }
+
+  @Patch(':id') //PATCH /users/:id
+  update(@Param('id',ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto:UpdateUserDto) {
+    return this.usersService.updated(id,updateUserDto);
+  }
+
+  @Delete(':id') //DELETE /users/:id
+  delete(@Param('id',ParseIntPipe) id: number) {
+    return this.usersService.delete(id)
+  }
+}
