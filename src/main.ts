056c81fe49c8d2b2088b4739c4d252629f2c5ae1
majdyaq <majd@feelit.tech>
2024-02-18 14:24:41 +0200

    enable cors and add api prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //open api to all
+  app.enableCors();
+  //add api prefix
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
